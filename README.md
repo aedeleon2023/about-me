5623b39bfdc667cc6e10afe829c47c5e26832ace
Angela DeLeon <aedeleon2023@gmail.com>
2022-07-13 23:41:54 -0700

    updating for lab 3.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # About Me
+
 ## LAB - 2
 
 ### Author: Angela DeLeon
--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
 
-let siteGuest = prompt('So, what is your name?');
+/*let siteGuest = prompt('So, what is your name?');
 alert(`Hello, ${siteGuest}! Ready to play a game?`);
 alert('I hope that was a yes, because there is no no.');
-alert('I like to call this game, How Well Do You Know Me? Rules are simple - Answer Yes or No.')
+alert('I like to call this game, How Well Do You Know Me?');
 
 let questionOne = prompt('Do I have cats?').toLowerCase();
 
@@ -41,7 +41,43 @@ let questionFour = prompt('Do I love fajitas?').toLowerCase();
 if (questionFour === 'y' || questionFour === 'yes') {
   alert(`Damn right, ${siteGuest}!`);
 } else if (questionFour === 'n' || questionFour === 'no') {
-  alert(`You don\'t know me at all, ${siteGuest}!`);
+  alert(`You don't know me at all, ${siteGuest}!`);
 } else {
   alert('Please answer Yes or No.');
+} */
+
+
+let chance = 4;
+let random = Math.floor(Math.random() * 100 + 1);
+console.log(random);
+let num;
+do {
+
+  num = prompt('What\'s my favorite number?  Hint, it\'s between 1 and 100.') * 1;
+  chance--;
+  if (isNaN(num) || num > 100 || num < 1) {
+
+    chance !== 0 ? alert(` Please enter the correct number , And then there were ${chance} Time `) : alert(' Game over ');
+  } else if (num > random) {
+
+    chance !== 0 ? alert(`Too big! You have ${chance} chances left!`) : alert(' Game over ');
+  } else if (num < random) {
+
+    chance !== 0 ? alert(`Too small! You have ${chance} chances left!`) : alert(' Game over ');
+  } else {
+
+    alert('Woot! Woot!  You Got It!');
+    break;
+  }
+} while (random !== num && chance > 0);
+
+
+let questionSix = prompt('What\'s my favorite color?').toLowerCase();
+
+if (questionSix === ) {
+  alert(`You really get me, ${siteGuest}!`);
+} else if (questionSix === ) {
+  alert(`That's a nice color, but not my favorite, ${siteGuest} .  Try again.`);
+} else {
+  alert('Yeah, I don\'t know about that color.');
 }
